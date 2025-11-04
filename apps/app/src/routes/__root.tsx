4ec36e9d494afb764e26e41aad88edd0e72426b2
Dang Van Thanh <dangvanthanh@dangthanh.org>
2025-11-03 18:14:57 -0800

    chore: update root for solid

--- apps/app/src/routes/__root.tsx
+++ apps/app/src/routes/__root.tsx
@@ -1,16 +1,15 @@
 import {
 	createRootRouteWithContext,
 	HeadContent,
-	Outlet,
 	Scripts,
 } from '@tanstack/solid-router'
 import { TanStackRouterDevtools } from '@tanstack/solid-router-devtools'
-import { Suspense } from 'solid-js'
 import { HydrationScript } from 'solid-js/web'
+import type * as Solid from 'solid-js'
 
 import Header from '../components/Header'
 
-import styles from '../styles/app.css?url'
+import appCss from '../styles/app.css?url'
 
 export const Route = createRootRouteWithContext()({
 	head: () => ({
@@ -19,24 +18,43 @@ export const Route = createRootRouteWithContext()({
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 			{ title: 'TanStack, Solid and Turborepo Boilerplate' },
 		],
-		links: [{ rel: 'stylesheet', href: styles }],
+		links: [
+			{ rel: 'stylesheet', href: appCss },
+			{
+				rel: 'apple-touch-icon',
+				sizes: '180x180',
+				href: '/apple-touch-icon.png',
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '32x32',
+				href: '/favicon-32x32.png',
+			},
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '16x16',
+				href: '/favicon-16x16.png',
+			},
+			{ rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
+			{ rel: 'icon', href: '/favicon.ico' },
+		],
 	}),
 	shellComponent: RootComponent,
 })
 
-function RootComponent() {
+function RootComponent(props: { children: Solid.JSX.Element }) {
 	return (
 		<html lang="en">
 			<head>
-				<HeadContent />
 				<HydrationScript />
 			</head>
 			<body>
-				<Suspense>
-					<Header />
-					<Outlet />
-					<TanStackRouterDevtools />
-				</Suspense>
+				<HeadContent />
+				<Header />
+				{props.children}
+				<TanStackRouterDevtools />
 				<Scripts />
 			</body>
 		</html>
